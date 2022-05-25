134fd16d16d4cbfc08aee1ab8a6d5326923d3c4f
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-25 09:00:33 -0400

    modified import from data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { data } from '/js/data.js';
+import { data } from './data.js';
 
 const source = document.querySelector('#result-template').innerHTML;
 const template = Handlebars.compile(source);
